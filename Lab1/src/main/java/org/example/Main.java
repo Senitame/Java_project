9338db70786de4579af03e70b1d6a7921ff88e17
Nit <146004864+senitame@users.noreply.github.com>
2023-11-01 09:48:33 +0200

    add lab 2

--- Lab1/src/main/java/org/example/Main.java
+++ Lab1/src/main/java/org/example/Main.java
@@ -3,10 +3,28 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Engine engine = new Engine(998, 200);
-        Motorcycle motorcycle = new Motorcycle("Kawasaki", "Nija H2 SX", 2022, 37806.84, engine);
+        Engine engine1 = new Engine(313, 34);
+        Motorcycle motorcycle = new Motorcycle("Kawasaki", "Nija H2 SX", 2022, 378006.84, engine);
+        Motorcycle motorcycle1 = new Motorcycle("BMW", "G310R", 2018, 178075.65, engine);
         Customer customer = new Customer("Yuri Plisetsky");
         Dealer dealer = new Dealer();
 
         dealer.sell(motorcycle, customer);
+        System.out.println();
+
+        //toString()
+        System.out.println("Motorcycle info 1: " + motorcycle.toString());
+        System.out.println("Motorcycle info 2: " + motorcycle1.toString());
+        System.out.println();
+        //hashCode()
+        System.out.println("Motorcycle hash code 1: " + motorcycle.hashCode());
+        System.out.println("Motorcycle hash code 2: " + motorcycle1.hashCode());
+        System.out.println();
+        //equals()
+        if (motorcycle.equals(motorcycle1)) {
+            System.out.println("Motorcycles are the same.");
+        } else {
+            System.out.println("Motorcycles are different.");
+        }
     }
 }
--- Lab1/src/main/java/org/example/Motorcycle.java
+++ Lab1/src/main/java/org/example/Motorcycle.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.util.Objects;
+
 class Motorcycle extends Vehicle implements saleOfGoods {
-    private final double price;
-    private Engine engine;
+    private final Double price;
+    private final Engine engine;
     public Motorcycle(String brand, String model, int year, double price, Engine engine) {
         super(brand, model, year);
         this.price = price;
@@ -17,6 +19,22 @@ class Motorcycle extends Vehicle implements saleOfGoods {
         System.out.println("Year of issue: " + year);
         System.out.println("Price: $" + calculatePrice());
         System.out.println("Engine: " + engine.getDisplacement() + " sm^3, " + engine.getHorsePower() + " h.p.");
+    }
+    @Override
+    public String toString() {
+        return "Motorcycle: " + year + " " + brand + " " + model;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Motorcycle)) return false;
+        Motorcycle motorcycle = (Motorcycle) o;
+        return year == motorcycle.year &&
+                Objects.equals(brand, motorcycle.brand) &&
+                Objects.equals(model, motorcycle.model);    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, year);
     }
 
     @Override
